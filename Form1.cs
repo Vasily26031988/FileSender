1f80977c0b27a828097847c5eba8de75b5dd66e8
Василий Василий <sator1988@gmail.com>
2021-02-02 18:19:10 +0400

    передали корректно строки с помощью Regex

--- Form1.cs
+++ Form1.cs
@@ -6,29 +6,45 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using YandexDisk.Client;
+using YandexDisk.Client.Clients;
+using YandexDisk.Client.Http;
 
 namespace FileSender
 {
 	public partial class Form1 : Form
 	{
-		
+		private string _path;
+
+		public string Path
+		{
+			get { return _path; }
+			set { _path = value; }
+		}
+
+
 		public Form1()
 		{
 			InitializeComponent();
 		}
+
+
 
 		void panel1_DragEnter(object sender, DragEventArgs e)
 		{
 			if (e.Data.GetDataPresent(DataFormats.FileDrop))
 			{
 				e.Effect = DragDropEffects.Copy;
-			}			
+			}
 		}
 
 		private void panel1_DragDrop(object sender, DragEventArgs e)
 		{
+
 			List<string> paths = new List<string>();
 
 			foreach (string obj in (e.Data.GetData(DataFormats.FileDrop) as string[]))
@@ -41,17 +57,62 @@ namespace FileSender
 				{
 					paths.Add(obj);
 				}
-				label1.Text += string.Join(Environment.NewLine, paths) + Environment.NewLine;
+				label1.Text += string.Join(Environment.NewLine, paths) +" ";
+				
+				//Path = string.Join(Environment.NewLine, paths) + " ";
+				Regex regex = new Regex(@"\r\n", RegexOptions.Multiline);
+				MatchCollection matches = regex.Matches(label1.Text);
+				if (matches.Count>0)
+				{
+					foreach (var match in matches)
+					{
+						//Path = string.Join(Environment.NewLine, paths) + " ";
+						string pattern = @"\r\n";
+						string target = " ";
+						Regex regexReplacement = new Regex(pattern);
+						Path = regexReplacement.Replace(label1.Text, target);
+					}
+				}
+
+
+				//label1.Text += $@"{string.Join("\n\r", paths)} ";
+				//MessageBox.Show(Path);
+				//Path = label1.Text;
+				//Path = string.Join(Environment.NewLine, paths) + Environment.NewLine; 
 			}
 		}
-
-		
 		private void button2_Click(object sender, EventArgs e)
-		{			
-			
-			
-			string [] writePath = new string[] { };
-			
+		{
+
+			//StreamReader file = new StreamReader(Path, Encoding.Default);
+			//string str = file.ReadToEnd();
+
+			string[] readPaths = File.ReadAllLines(Path, Encoding.Default);
+
+
+
+
+		}
+
+		async Task UploadSample()
+		{
+			//You should have oauth token from Yandex Passport.
+			//See https://tech.yandex.ru/oauth/
+			string oauthToken = "<token hear>";
+
+			// Create a client instance
+			IDiskApi diskApi = new DiskHttpApi(oauthToken);
+
+			//Upload file from local
+			await diskApi.Files.UploadFileAsync(path: "/foo/myfile.txt",
+				overwrite: false,
+				localFile: @"C:\myfile.txt",
+				cancellationToken: CancellationToken.None);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
