bb0a1ae4a3f896afa554e07ab0a7b08e0af0481f
Василий Василий <sator1988@gmail.com>
2021-02-10 23:20:45 +0400

    Write paths to file

--- Form1.cs
+++ Form1.cs
@@ -32,8 +32,6 @@ namespace FileSender
 			InitializeComponent();
 		}
 
-
-
 		void panel1_DragEnter(object sender, DragEventArgs e)
 		{
 			if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -44,24 +42,6 @@ namespace FileSender
 
 		private void panel1_DragDrop(object sender, DragEventArgs e)
 		{
-
-			string path;
-			if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\source\repos\FileSender\TempReadedFileOfPath.txt"))
-			{
-				FileStream file = new FileStream($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\source\repos\FileSender\TempReadedFileOfPath.txt", FileMode.OpenOrCreate);			
-			}
-			else if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\source\repos\FileSender\TempReadedFileOfPath.txt"))
-			{
-				FileStream file = new FileStream($@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\source\repos\FileSender\TempReadedFileOfPath.txt", FileMode.OpenOrCreate);
-			}
-			else
-			{
-				FileStream file = new FileStream($@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\source\repos\FileSender\TempReadedFileOfPath.txt", FileMode.OpenOrCreate);
-			}
-					
-			StreamWriter stream = new StreamWriter("");
-
-			
 			List<string> paths = new List<string>();
 			foreach (string obj in (e.Data.GetData(DataFormats.FileDrop) as string[]))
 			{
@@ -73,54 +53,67 @@ namespace FileSender
 				{
 					paths.Add(obj);
 				}
-				label1.Text += string.Join(Environment.NewLine, paths) + " ";
-
-
-
-				Regex regex = new Regex(@"\r\n", RegexOptions.Multiline);
-				MatchCollection matches = regex.Matches(label1.Text);
-				if (matches.Count > 0)
-				{
-					foreach (var match in matches)
-					{
-
-						//Path = string.Join(Environment.NewLine, paths) + " ";
-						string pattern = @"\r\n";
-						string target = " ";
-						Regex regexReplacement = new Regex(pattern);
-						Path = regexReplacement.Replace(label1.Text, target);
-
-
-
-					}
-				}
-
-
+				label1.Text += string.Join(Environment.NewLine, paths) + Environment.NewLine;
 			}
-
+			RecordPaths();
 		}
 
-			
+		void RecordPaths()
+		{
+			if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\source\repos\FileSender\TempReadedFileOfPath.txt"))
+			{
+				Path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\source\repos\FileSender\TempReadedFileOfPath.txt";
+				FileStream file = new FileStream($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\source\repos\FileSender\TempReadedFileOfPath.txt", FileMode.OpenOrCreate);
+				StreamWriter stream = new StreamWriter(file);
+				stream.WriteLine(label1.Text);
+				stream.Close();
+				file.Close();
+			}
+			else if (File.Exists($@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\source\repos\FileSender\TempReadedFileOfPath.txt"))
+			{
+				Path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\source\repos\FileSender\TempReadedFileOfPath.txt";
+				FileStream file = new FileStream($@"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}\source\repos\FileSender\TempReadedFileOfPath.txt", FileMode.OpenOrCreate);
+				StreamWriter stream = new StreamWriter(file);
+				stream.WriteLine(label1.Text);
+				stream.Close();
+				file.Close();
+			}
+			else
+			{
+				Path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)}\source\repos\FileSender\TempReadedFileOfPath.txt";
+				FileStream file = new FileStream(Path, FileMode.OpenOrCreate);
+				StreamWriter stream = new StreamWriter(file);
+				stream.WriteLine(label1.Text);
+				stream.Close();
+				file.Close();
+			}
+		}
+
+
+
+
 
 		private void button2_Click(object sender, EventArgs e)
 		{
 			ReadDirectory();
-
 		}
 
 
 
 		void ReadDirectory()
 		{
-			string path = @"C:\Users\gololobovVV\Desktop\папка";
+			StreamReader readedPaths = new StreamReader(Path);
+			string[] readPaths = File.ReadAllLines(Path);
+			readedPaths.Close();
+
 
 			//foreach (string dir in Directory.GetDirectories(path))
 			//{
-			foreach (string file in Directory.GetFiles(path))
+			foreach (string file in Directory.GetFiles(""))
 			{
-				label3.Text += file + "\n";
+				//label3.Text += file + "\n";
 
-				File.ReadAllLines(label3.Text);
+				//File.ReadAllLines(label3.Text);
 
 			}
 			//}
@@ -130,11 +123,6 @@ namespace FileSender
 
 		async Task UploadSample()
 		{
-
-
-
-
-
 
 			//You should have oauth token from Yandex Passport.
 			//See https://tech.yandex.ru/oauth/
@@ -146,7 +134,7 @@ namespace FileSender
 			//Upload file from local
 			await diskApi.Files.UploadFileAsync(path: $@"123.txt",
 				overwrite: false,
-				localFile: @"C:\Users\gololobovVV\Desktop\папка\123.txt",
+				localFile: @"C:\Users\sator\Desktop\123.txt",
 				cancellationToken: CancellationToken.None);
 		}
 
